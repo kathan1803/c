b0514976a41892395da6e17b404b67ad88d348bd
Kathan Patel <109129990+kathan1803@users.noreply.github.com>
2023-02-16 11:12:27 +0530

    Delete deep.py

--- deep.py
+++
@@ -1,9 +0,0 @@
-x = input("What is the answer to the great question of life, the universe and everything? ")
-if x.replace(" ", "") == "42":
-    print("Yes")
-elif x.lower() == "forty-two":
-    print("Yes")
-elif x.lower() == "forty two":
-    print("Yes")
-else:
-    print("No")
